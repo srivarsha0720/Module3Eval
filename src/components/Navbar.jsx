b4c6393ced06583cddc7e12da78cf4c4af982925
srivarsha0720 <varshapabba232gmail.com>
2026-01-03 20:51:27 +0530

    mini-restaurant-management-app

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,19 @@
-import {useRef} from "react";
+import React from "react";
 const Navbar=({search,setSearch,type,setType,parking,setParking})=>{
-  const ref=useRef(null);
     return(
         <div>
-            <input ref={ref} placeholder="Search restaurant" value={search} onChange={(e)=>setSearch(e.target.value)}/>
-        <select value={type} onChange={(e)=>setType(e.target.value)}/>
+            <input placeholder="Search restaurant" value={search} onChange={(e)=>setSearch(e.target.value)}/>
+        <select value={type} onChange={(e)=>setType(e.target.value)}>
             <option value="">All Types</option>
-            <option>Rajasthani</option>
-              <option>Gujarati</option>
-                <option>Mughlai</option>
-                  <option>Jain</option>
-                    <option>Thai</option>
-                      <option>North Indian</option>
-                      <option>South Indian</option>
+            <option value="Rajasthani">Rajasthani</option>
+              <option value="Gujarati">Gujarati</option>
+                <option value="Mughlai">Mughlai</option>
+                  <option value="Jain">Jain</option>
+                    <option value="Thai">Thai</option>
+                      <option value="North Indian">North Indian</option>
+                      <option value="South Indian">South Indian</option>
         </select>
-             <select value={parking} onChange={(e)=>setParking(e.target.value)}/>
+             <select value={parking} onChange={(e)=>setParking(e.target.value)}>
             <option value="">All</option>
             <option value="true">parking</option> 
             <option value="false">no parking</option> 
